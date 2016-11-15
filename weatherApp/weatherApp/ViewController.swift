367056ff47ddbc46a04a3130cd1ba833dff5eee1
surnan <admin@it-admins-macbook-air.local>
2016-11-15 07:30:31 -0500

    first

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by admin on 11/15/16.
+//  Copyright © 2016 UPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
